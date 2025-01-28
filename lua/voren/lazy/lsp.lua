1a42ab066777c4491650ffb37404cf9bb484d53d
Voren <150689455+vorenxiii@users.noreply.github.com>
2025-01-28 14:09:06 +0100

    Comment out go/rust lsp

--- lua/voren/lazy/lsp.lua
+++ lua/voren/lazy/lsp.lua
@@ -32,9 +32,9 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
-                "gopls",
                 "clangd",
+                -- "rust_analyzer",
+                -- "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -57,7 +57,6 @@ return {
                     })
                     vim.g.zig_fmt_parse_errors = 0
                     vim.g.zig_fmt_autosave = 0
-
                 end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
